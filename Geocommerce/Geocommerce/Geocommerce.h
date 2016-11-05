4fe64af9bfa3aa9e50f627f44c4349f42104795f
rmarathay <rmarathay@gmail.com>
2016-11-05 14:53:32 -0400

    creating xcode project

---
+++ Geocommerce/Geocommerce/Geocommerce.h
@@ -0,0 +1,19 @@
+//
+//  Geocommerce.h
+//  Geocommerce
+//
+//  Created by Ranjit Marathay on 11/5/16.
+//  Copyright © 2016 Ranjit Marathay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geocommerce.
+FOUNDATION_EXPORT double GeocommerceVersionNumber;
+
+//! Project version string for Geocommerce.
+FOUNDATION_EXPORT const unsigned char GeocommerceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geocommerce/PublicHeader.h>
+
+
