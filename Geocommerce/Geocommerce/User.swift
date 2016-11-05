a548b0389ad7064d9fe01bd2badc670cee24f261
rmarathay <rmarathay@gmail.com>
2016-11-05 15:48:32 -0400

    Hitting road blocks

---
+++ Geocommerce/Geocommerce/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  Geocommerce
+//
+//  Created by Ranjit Marathay on 11/5/16.
+//  Copyright © 2016 Ranjit Marathay. All rights reserved.
+//
+
+import Foundation
+
+
+class User{
+    var userId = String()
+    
+    init(init_userId: String){
+        self.userId = init_userId;
+    }
+    
+    
+    func collectData(){
+        
+    }
+    
+    
+    
+
+
+}
